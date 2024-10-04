5ec9d7d46dcbefc581c6bedc7b70fb69fe48a92c
uzair1709 <uzairo.khan2010@gmail.com>
2024-10-04 16:08:04 +0300

    Delete file13.py

--- file13.py
+++
@@ -1,8 +0,0 @@
-import requests
-def get_location(ip_address):
-    response=requests.get(f"http://ip-api.com/json/{ip_address}")
-    data=response.json()
-    return data
-ip_address="37.186.36.25"
-location=get_location(ip_address)
-print(f"country:{location["country"]}")
