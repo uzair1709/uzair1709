91de39e6cf4d75069019e9c190c98b3720fa77b1
uzair1709 <uzairo.khan2010@gmail.com>
2024-10-04 15:59:09 +0300

    Add files via upload

---
+++ print22.py
@@ -0,0 +1,7 @@
+current_year=int(input("plzz enter your current year "))
+birth_year=int(input("plzz enter your birth year "))
+age= current_year-birth_year
+if age <18:
+    print("you are a teenager ")
+else: 
+    print("you are an adult")   
